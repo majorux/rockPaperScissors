53b845e4c8260babc27e52a7aa9d8aad4478fc16
farex2 <farabd90@gmail.com>
2022-08-16 12:56:56 +0100

    add a compare funtion to compare the answers

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function getPlayerSelection(){
   let choice = answer.toLowerCase();
   return choice
 }
+
+const playerSeletion = getPlayerSelection();
+const computerSelection =  getComputerChoice();
+function compare(choiceA, choiceB){
+  if (choiceA == "rock" && choiceB == "paper"){
+    return "paper";
+  } else if (choiceA == "rock" && choiceB == "scissors"){
+    return "rock";
+  } else if (choiceA == "paper" && choiceB == "scissors"){
+    return "scissors";
+  } else {
+    return "draw";
+  }
+}
+
+function playround(playerSeletion, computerSelection){
+
+}
