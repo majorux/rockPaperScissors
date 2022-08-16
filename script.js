97004e003780d9914863a5d48a3c20842118371c
farex2 <farabd90@gmail.com>
2022-08-16 12:11:31 +0100

    add a funtion to get user input and convert it to lower case

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function getComputerChoice() {
-  choices = ["rock", "paper", "scissors"]
+  choices = ["rock", "paper", "scissors"];
   randomElement = choices[Math.floor(Math.random() * choices.length)];
   return randomElement
+}
+
+function getPlayerSelection(){
+  let answer = prompt("enter choice");
+  let choice = answer.toLowerCase();
+  return choice
 }
