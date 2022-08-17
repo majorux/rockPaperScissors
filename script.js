914a1dc9cfc97de72e9fd109f6555e43f4624fd9
farex2 <farabd90@gmail.com>
2022-08-17 10:14:14 +0100

    add a playround() funtion to check who wins

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-  choices = ["rock", "paper", "scissors"];
-  randomElement = choices[Math.floor(Math.random() * choices.length)];
+  let choice = ["rock", "paper", "scissors"];
+  randomElement = choice[Math.floor(Math.random() * choice.length)];
   return randomElement
 }
 
@@ -12,18 +12,40 @@ function getPlayerSelection(){
 
 const playerSeletion = getPlayerSelection();
 const computerSelection =  getComputerChoice();
+const choices = ["rock", "paper", "scissors"];
+
 function compare(choiceA, choiceB){
-  if (choiceA == "rock" && choiceB == "paper"){
-    return "paper";
-  } else if (choiceA == "rock" && choiceB == "scissors"){
-    return "rock";
-  } else if (choiceA == "paper" && choiceB == "scissors"){
-    return "scissors";
+  if (choices.includes(choiceA) && choices.includes(choiceB)){
+    if (choiceA === "rock" && choiceB === "paper"){
+      return "paper"
+    } else if (choiceA === "rock" && choiceB === "scissors"){
+      return "rock"
+    } else if (choiceA === "paper" && choiceB === "scissors"){
+      return "scissors"
+    }  else if (choiceA === "paper" && choiceB === "rock"){
+      return "paper"
+    }  else if (choiceA === "scissors" && choiceB === "rock"){
+      return "rock"
+    }  else if (choiceA === "scissors" && choiceB === "paper"){
+      return "scissors"
+    } else {
+      return "draw"
+    }
   } else {
-    return "draw";
+    return "wrong input"
   }
 }
 
-function playround(playerSeletion, computerSelection){
-
+function playround(player, computer){
+  if (compare(player, computer) === player){
+    console.log("you win, " + playerSeletion + " beats " + computerSelection)
+  } else if (compare(playerSeletion,computerSelection) == "draw"){
+    console.log("its a draw")
+  } else if (compare(player, computer) === computer){
+    console.log("you lose, " + computerSelection + " beats " + playerSeletion)
+  } else {
+    return "something went wrong"
+  }
 }
+playround(playerSeletion, computerSelection)
+console.log(playerSeletion, computerSelection)
